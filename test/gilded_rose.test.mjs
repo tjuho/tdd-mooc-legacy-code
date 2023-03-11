5eb89b78962bc577d1ea0d878ab2e212abdd6c3e
Juho Taipale <krooh@hotmail.com>
2023-03-11 11:53:54 +0200

    Aged Brie 00 quality test

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -29,7 +29,7 @@ describe("Gilded Rose coverage tests", () => {
   });
 });
 
-describe("Gilded Rose quality", () => {
+describe("Gilded Rose foo quality", () => {
   it("should foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
@@ -50,14 +50,12 @@ describe("Gilded Rose quality", () => {
     const items = gildedRose.updateQuality();
     expect(items[0].quality).to.equal(9);
   });
-  // it("should Aged Brie", () => {
-    // const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
-    // const items = gildedRose.updateQuality();
-    // expect(items[0].name).to.equal("Aged Brie");
-  // });
-  // it("should Backstage passes to a TAFKAL80ETC concert", () => {
-    // const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
-    // const items = gildedRose.updateQuality();
-    // expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
-  // });
+});
+
+describe("Gilded Rose Aged Brie quality", () => {
+  it("should foo", () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(2);
+  });
 });
