92f60316edfdd45ab970c42afb668c62fe348156
Juho Taipale <krooh@hotmail.com>
2023-03-11 11:36:45 +0200

    foo0 quality test

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
-describe("Gilded Rose", () => {
+describe("Gilded Rose coverage tests", () => {
   it("should foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
@@ -27,4 +27,32 @@ describe("Gilded Rose", () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
   });
+});
+
+describe("Gilded Rose quali", () => {
+  it("should foo", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0);
+  });
+  // it("should foo with quality", () => {
+    // const gildedRose = new Shop([new Item("foo", 0, 1)]);
+    // const items = gildedRose.updateQuality();
+    // expect(items[0].name).to.equal("foo");
+  // });
+  // it("should foo with s-10 q10", () => {
+    // const gildedRose = new Shop([new Item("foo", -10, 10)]);
+    // const items = gildedRose.updateQuality();
+    // expect(items[0].name).to.equal("foo");
+  // });
+  // it("should Aged Brie", () => {
+    // const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
+    // const items = gildedRose.updateQuality();
+    // expect(items[0].name).to.equal("Aged Brie");
+  // });
+  // it("should Backstage passes to a TAFKAL80ETC concert", () => {
+    // const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
+    // const items = gildedRose.updateQuality();
+    // expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+  // });
 });
