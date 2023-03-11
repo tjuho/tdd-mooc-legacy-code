eaba0f2db04c2b6c4628a09ea181f63cf411706c
Juho Taipale <krooh@hotmail.com>
2023-03-11 11:20:35 +0200

    Backstage passes to a TAFKAL80ETC concert test

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -12,4 +12,9 @@ describe("Gilded Rose", () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("Aged Brie");
   });
+  it("should Backstage passes to a TAFKAL80ETC concert", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+  });
 });
