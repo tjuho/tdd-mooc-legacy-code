3d9a4abfd8dac6966244df24980996f5f956b90a
Juho Taipale <krooh@hotmail.com>
2023-03-12 13:46:51 +0200

    foo ready and tests pass

--- src/gilded_rose_refactored.mjs
+++ src/gilded_rose_refactored.mjs
@@ -14,7 +14,12 @@ export class Shop {
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name == "Aged Brie"){
-        this.items[i].quality += 1;
+        this.items[i].sellIn -= 1;
+        if (this.items[i].sellIn < 0){
+          this.items[i].quality += 2;
+        } else {
+          this.items[i].quality += 1;
+        }
       } else if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"){
         if (this.items[i].sellIn < 1){
           this.items[i].quality = 0;
@@ -26,9 +31,14 @@ export class Shop {
           this.items[i].quality += 1;
         }
       } else if (this.items[i].name == "Sulfuras, Hand of Ragnaros"){
-        
+        continue;
       } else {
-        
+        this.items[i].sellIn -= 1;
+        if (this.items[i].sellIn < 0){
+          this.items[i].quality -= 2;
+        } else {
+          this.items[i].quality -= 1;
+        }
       }
       if (this.items[i].quality > 50){
         this.items[i].quality = 50;
