d1ae7a2b6928f9c87984ccacb41c36d6c092ccea
Juho Taipale <krooh@hotmail.com>
2023-03-12 13:53:45 +0200

    moved 'sellIn -1 to front of if else

--- src/gilded_rose_refactored.mjs
+++ src/gilded_rose_refactored.mjs
@@ -13,8 +13,11 @@ export class Shop {
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
+      if (this.items[i].name == "Sulfuras, Hand of Ragnaros"){
+        continue;
+      }
+      this.items[i].sellIn -= 1;
       if (this.items[i].name == "Aged Brie"){
-        this.items[i].sellIn -= 1;
         if (this.items[i].sellIn < 0){
           this.items[i].quality += 2;
         } else {
@@ -23,17 +26,14 @@ export class Shop {
       } else if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"){
         if (this.items[i].sellIn < 1){
           this.items[i].quality = 0;
-        } else if (this.items[i].sellIn < 6){
+        } else if (this.items[i].sellIn < 5){
           this.items[i].quality += 3;
-        } else if (this.items[i].sellIn < 11){
+        } else if (this.items[i].sellIn < 10){
           this.items[i].quality += 2
         } else {
           this.items[i].quality += 1;
         }
-      } else if (this.items[i].name == "Sulfuras, Hand of Ragnaros"){
-        continue;
       } else {
-        this.items[i].sellIn -= 1;
         if (this.items[i].sellIn < 0){
           this.items[i].quality -= 2;
         } else {
