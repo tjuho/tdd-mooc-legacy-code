53c66cbb6ba848e32fd93c0347c4bda4407b8d39
Juho Taipale <krooh@hotmail.com>
2023-03-12 13:37:43 +0200

    backstage passes now pass

--- src/gilded_rose_refactored.mjs
+++ src/gilded_rose_refactored.mjs
@@ -14,14 +14,33 @@ export class Shop {
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name == "Aged Brie"){
-        
+        this.items[i].quality += 1;
       } else if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"){
-        
+        if (this.items[i].sellIn < 1){
+          this.items[i].quality = 0;
+        } else if (this.items[i].sellIn < 6){
+          this.items[i].quality += 3;
+        } else if (this.items[i].sellIn < 11){
+          this.items[i].quality += 2
+        } else {
+          this.items[i].quality += 1;
+        }
       } else if (this.items[i].name == "Sulfuras, Hand of Ragnaros"){
         
       } else {
         
       }
+      if (this.items[i].quality > 50){
+        this.items[i].quality = 50;
+      } else if (this.items[i].quality < 0){
+        this.items[i].quality = 0;
+      }
+    }
+    return this.items;
+  }
+  
+  updateQuality1() {
+    for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
